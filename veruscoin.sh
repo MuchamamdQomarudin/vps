8d35454e2594d1b35d612cec6ea34e5d680e55b0
MuchamamdQomarudin <84380279+MuchamamdQomarudin@users.noreply.github.com>
2021-05-18 17:36:25 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,7 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RM92DPQ8vdvx4CNNzNqLs9YUZFFNYy8b9K.JAYA -p x --cpu 4
